1a1ee45feb6209561eb2b08db0cb4adfeb459627
dearxiaoda <540383953@qq.com>
2019-06-30 16:58:50 +0800

    样式调整

--- src/components/personal/MyAccount.vue
+++ src/components/personal/MyAccount.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="account-wrap">
+  <div class="account-wrap" style="max-width:830px">
     <div class="title">
       <img src="../../assets/images/person/person.png" />Connnect Account
     </div>
-    <el-form ref="form" :model="form" label-width="120px">
+    <el-form ref="form" :model="form" label-width="160px">
       <el-form-item label="First Name">
         <el-input v-model="form.firstName"></el-input>
       </el-form-item>
@@ -27,31 +27,15 @@
           <el-input v-model.number="form.day" placeholder="请输入内容"></el-input>
         </el-col>
         <el-col class="el-form--label-left cosmi-datastyle" :span="4">Day</el-col>
-        </el-col>
       </el-form-item>
-      <el-form-item label="即时配送">
-        <el-switch v-model="form.delivery"></el-switch>
+      <el-form-item label="Telephone Number">
+        <el-input v-model.number="form.phone"></el-input>
       </el-form-item>
-      <el-form-item label="活动性质">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="特殊资源">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="活动形式">
-        <el-input type="textarea" v-model="form.desc"></el-input>
+      <el-form-item label="Subscription Mail">
+        <el-input v-model.number="form.phone"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        <el-button>取消</el-button>
+        <el-button type="primary" @click="onSubmit">SAVE</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -67,7 +51,7 @@ export default {
         year: '',
         month: '',
         day: '',
-        delivery: false,
+        phone: '',
         type: [],
         resource: '',
         desc: ''
@@ -93,7 +77,7 @@ export default {
       vertical-align: middle;
     }
   }
-  .cosmi-datastyle{
+  .cosmi-datastyle {
     padding-left: 12px;
   }
 }
